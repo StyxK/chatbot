a1499b5fdaa5f2f66ccbd3936c0a2f9629fc654e
StyxK <nosomething050@gmail.com>
2019-06-28 00:38:09 +0700

    [ADD] intent word in sentense using wit.ai

--- server.js
+++ server.js
@@ -1,6 +1,32 @@
 const express = require('express')
 const app = express()
+const config =  require('dotenv').config()
+const {Wit,log} =  require('node-wit')
 
-app.listen(3000,()=>{
-    console.log("hello chatbot")
+const client = new Wit({
+    accessToken : process.env.TOKEN,
+    actions : {
+        send(req,res){
+            return new Promise((resolve,reject)=>{
+                console.log(JSON.stringify(res))
+                return resolve
+            })
+        },
+        myAction({sessionId,context,text,entities}){
+            console.log(`Session ${sessionId} recieved ${text}`)
+            console.log(`The current context is ${JSON.stringify(context)}`)
+            console.log(`Wit extracted ${JSON.stringify(entities)}`)
+            return Promise.resolve(context)
+        }
+    },
+    logger : new log.Logger(log.DEBUG)
+})
+
+console.log(client.message('I need a sausage pizza'))
+
+const sessionId = 'my-user-session-42'
+const context0 = {}
+
+app.listen(process.env.PORT,()=>{
+    console.log(`hello chatbot on port ${process.env.PORT}`)
 })
